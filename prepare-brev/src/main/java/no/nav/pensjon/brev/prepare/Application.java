61a3df1ef0b5fdfbf242802b20da72bd015ce64c
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2020-10-16 10:19:48 +0200

    PL-2504 Prepare-brev skeleton setup + Github Actions deploy

---
+++ prepare-brev/src/main/java/no/nav/pensjon/brev/prepare/Application.java
@@ -0,0 +1,15 @@
+package no.nav.pensjon.brev.prepare;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
