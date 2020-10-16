3450804ea7062f0b5ef633e6a51a7ac911ea2fe4
dainius <dainiusfigoras@gmail.com>
2020-10-16 09:48:50 +0200

    PL-2469 fetching all brev records from brevmetadata and endpoint to test that

---
+++ brev-bestilling-boot/src/main/java/no/nav/pensjon/brev/bestilling/consumer/TestBrevMetaDataEndpoint.java
@@ -0,0 +1,20 @@
+package no.nav.pensjon.brev.bestilling.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestBrevMetaDataEndpoint {
+
+	@Autowired
+    BrevMetaData client;
+	
+	@GetMapping("/brevmetadata")
+	public List brevMetaData() throws Exception {
+		return client.hentAlleBrev(false);
+	}
+	
+}
