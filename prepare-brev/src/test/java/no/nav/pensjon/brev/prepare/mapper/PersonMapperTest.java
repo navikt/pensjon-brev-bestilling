a5b306c8b7af2e89c50e8d895a980a73d88aaf1f
Agne Hesten <Agne.Hesten@nav.no>
2020-11-02 12:28:08 +0100

    PL-2568: Changed java version to Java 11. Updated test to use constants

--- prepare-brev/src/test/java/no/nav/pensjon/brev/prepare/mapper/PersonMapperTest.java
+++ prepare-brev/src/test/java/no/nav/pensjon/brev/prepare/mapper/PersonMapperTest.java
@@ -12,21 +12,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PersonMapperTest {
     @Test
     void testPersonToPersonDto() {
+        final String FORNAVN = "Hans";
+        final String ETTERNAVN = "Hansen";
+        final String FULLNAVN = FORNAVN + " " + ETTERNAVN;
+        final String ADRESSE = "Sannergata 2 Oslo";
+        final LocalDate FODSELSDATO = LocalDate.of(1992, 12, 24);
+        final String EPOST = "hans.hansen@epost.no";
+        final String TELEFONNUMMER = "123456789";
+
         Person person = new Person();
-        person.setFornavn("Hans");
-        person.setEtternavn("Hansen");
-        person.setBostedsAdresse("Sannergata 2 Oslo");
-        person.setFodselsDato(LocalDate.of(1992, 12, 24));
+        person.setFornavn(FORNAVN);
+        person.setEtternavn(ETTERNAVN);
+        person.setBostedsAdresse(ADRESSE);
+        person.setFodselsDato(FODSELSDATO);
         Kontaktinformasjon kontaktinformasjon = new Kontaktinformasjon();
-        kontaktinformasjon.setEpost("hans.hansen@epost.no");
-        kontaktinformasjon.setTlfNummer("123456789");
+        kontaktinformasjon.setEpost(EPOST);
+        kontaktinformasjon.setTlfNummer(TELEFONNUMMER);
 
         PersonDto personDto = PersonMapper.MAPPER.personToPersonDto(person, kontaktinformasjon);
 
-        assertThat(personDto.getFullnavn()).isEqualTo("Hans Hansen");
-        assertThat(personDto.getAdresse()).isEqualTo("Sannergata 2 Oslo");
-        assertThat(personDto.getFodselsDato()).isEqualTo(LocalDate.of(1992, 12, 24));
-        assertThat(personDto.getEpost()).isEqualTo("hans.hansen@epost.no");
-        assertThat(personDto.getTelefonNummer()).isEqualTo("123456789");
+        assertThat(personDto.getFullnavn()).isEqualTo(FULLNAVN);
+        assertThat(personDto.getAdresse()).isEqualTo(ADRESSE);
+        assertThat(personDto.getFodselsDato()).isEqualTo(FODSELSDATO);
+        assertThat(personDto.getEpost()).isEqualTo(EPOST);
+        assertThat(personDto.getTelefonNummer()).isEqualTo(TELEFONNUMMER);
     }
 }
