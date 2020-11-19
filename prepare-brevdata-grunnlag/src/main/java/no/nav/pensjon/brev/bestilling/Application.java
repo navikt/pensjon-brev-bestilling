5b3f24593a7c5cde563a21f4500c0f9141a443a4
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2020-11-19 17:10:18 +0100

    demoløsning for kafka retry

--- prepare-brevdata-grunnlag/src/main/java/no/nav/pensjon/brev/bestilling/Application.java
+++ prepare-brevdata-grunnlag/src/main/java/no/nav/pensjon/brev/bestilling/Application.java
@@ -2,14 +2,19 @@ package no.nav.pensjon.brev.bestilling;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.endpoint.BindingsEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+import no.nav.pensjon.brev.bestilling.integrasjon.model.BrevErBestilt;
 import no.nav.pensjon.brev.bestilling.pdl.PdlClientConfig;
 import no.nav.pensjon.sts.client.StsClientConfig;
 
@@ -35,12 +40,59 @@ public class Application {
 		};
 	}
 
+
+
+	@Autowired
+	private BindingsEndpoint bindingsEndpoint;
+
+	private Set<String> attemptedRetryIds = new HashSet<>();
+
 	@Bean
-	public Function<String, String> brevErBestiltFunction() {
-		return value -> {
-			System.out.println("Received: " + value);
-			return value.toLowerCase();
+	public Function<BrevErBestilt, BrevErBestilt> brevErBestilt() {
+		return value -> processBrevErBestilt(value);
+	}
+
+	// Stops processing of DLT when the same message has been attempted twice
+	@Bean
+	public Function<BrevErBestilt, BrevErBestilt> brevErBestiltDlt() {
+		return brevErBestilt -> {
+			String messageId = brevErBestilt.getMeldingId();
+			if (attemptedRetryIds.contains(messageId)){
+				bindingsEndpoint.changeState("brevErBestiltDlt-in-0", BindingsEndpoint.State.STOPPED);
+			}
+
+			try {
+				return processBrevErBestilt(brevErBestilt);
+			}
+			catch (Exception e) {
+				attemptedRetryIds.add(messageId);
+				throw e;
+			}
 		};
 	}
 
+	/*
+		Dummy function to simulate errors happening. Can be removed.
+		Just here for illustration purposes.
+	 */
+	private int sometimesCount = 0;
+	private BrevErBestilt processBrevErBestilt(BrevErBestilt brevErBestilt) {
+
+		if (brevErBestilt.getBrevbestillingRequest() == null ||
+				brevErBestilt.getBrevbestillingRequest().getBrevKode() == null) {
+			return brevErBestilt;
+		}
+
+		if (brevErBestilt.getBrevbestillingRequest().getBrevKode().contains("sometimes")) {
+			sometimesCount++;
+			if (sometimesCount % 4 != 0) {
+				throw new RuntimeException("sometimes failed.");
+			}
+		}
+		else if (brevErBestilt.getBrevbestillingRequest().getBrevKode().contains("error")) {
+			throw new RuntimeException("error failed.");
+		}
+
+		return brevErBestilt;
+	}
 }
