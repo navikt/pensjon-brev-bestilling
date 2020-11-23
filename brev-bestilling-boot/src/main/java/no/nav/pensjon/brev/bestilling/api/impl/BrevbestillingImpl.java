177182a8dce2c334192ea73251f60a7aa8094dae
Alexander Petrov <alexander.petrov@nav.no>
2020-11-23 10:33:08 +0100

    fix compilation error

--- brev-bestilling-boot/src/main/java/no/nav/pensjon/brev/bestilling/api/impl/BrevbestillingImpl.java
+++ brev-bestilling-boot/src/main/java/no/nav/pensjon/brev/bestilling/api/impl/BrevbestillingImpl.java
@@ -3,7 +3,7 @@ package no.nav.pensjon.brev.bestilling.api.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.pensjon.brev.bestilling.integrasjon.api.BrevbestillingApiDelegate;
-import no.nav.pensjon.brev.bestilling.integrasjon.model.BrevbestillingRequest;
+import no.nav.pensjon.brev.bestilling.integrasjon.model.BrevBestillingRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +15,7 @@ public class BrevbestillingImpl implements BrevbestillingApiDelegate {
     private static final Logger logger = LoggerFactory.getLogger(BrevbestillingImpl.class);
 
         @Override
-    public ResponseEntity<Void> brevbestillingPost(BrevbestillingRequest brevbestillingRequest) {
+    public ResponseEntity<Void> brevbestillingPost(BrevBestillingRequest brevbestillingRequest) {
         try {
             logger.info(new ObjectMapper().writeValueAsString(brevbestillingRequest));
         } catch (JsonProcessingException e) {
