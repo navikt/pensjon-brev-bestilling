c976e49f3fa3cde6a9f55a3865c38677be6e3ff0
Kristian Laskemoen <Kristian.Laskemoen@nav.no>
2020-11-24 15:48:02 +0100

    PL-2503 Ryddet litt i koden

--- brev-bestilling-boot/src/main/java/no/nav/pensjon/brev/bestilling/Application.java
+++ brev-bestilling-boot/src/main/java/no/nav/pensjon/brev/bestilling/Application.java
@@ -23,35 +23,14 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	/**
-	 * The background function which triggers on an event from Pub/Sub and consumes the Pub/Sub
-	 * event message.
-	 */
-	@Bean
-	public Consumer<String> pubSubFunction() {
-		return message -> {
-			// The PubSubMessage data field arrives as a base-64 encoded string and must be decoded.
-			// See: https://cloud.google.com/functions/docs/calling/pubsub#event_structure
-			//String decodedMessage = new String(Base64.getDecoder().decode(message.getData()), StandardCharsets.UTF_8);
-			System.out.println("Received Pub/Sub message with data: " + message);
-		};
-	}
-
-//	@Bean
-//	public Supplier<byte[]> bestillBrevSupplier(){
-//		return () -> "TEST BREV_ER_BESTILLT".getBytes();
-//	}
-
 	@Autowired
-	public StreamBridge streamBridge;
+	private StreamBridge streamBridge;
 
 	@Bean
 	public Function<BrevbestillingRequest, String> brevbestilling(){
 		return (param1) -> {
 			streamBridge.send("brevbestilling-out-0",param1);
-			brevErBestillt();
 
-			System.out.println(param1);
 			return "antilope";
 		};
 	}
@@ -59,7 +38,6 @@ public class Application {
 	@Bean
 	public Function<String, String> brevErBestillt(){
 		return (param1) -> {
-			System.out.println("Da er vi her" + param1 + "erBestillt");
 			return param1;
 		};
 	}
